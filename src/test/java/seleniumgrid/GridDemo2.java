a284787b46948ab5f232f602dbc6970e0d3f6857
rachana1p <rachanaprakash2001@gmail.com>
2023-12-19 20:47:10 +0530

    first commit

---
+++ src/test/java/seleniumgrid/GridDemo2.java
@@ -0,0 +1,25 @@
+package seleniumgrid;
+
+
+import java.net.MalformedURLException;
+
+import org.testng.annotations.Test;
+
+public class GridDemo2 extends HubandNodeDemo {
+	
+	
+	@Test(priority='1')
+	public void chromeTest1() throws MalformedURLException
+	{
+		hubNodeChromeDemo();
+		driver.get("https://www.selenium.dev/downloads/");
+	}
+	
+	@Test(priority='2')
+	public void firefoxTest1() throws MalformedURLException
+	{
+		hubNodeFirefoxDemo();
+		driver.get("https://www.selenium.dev/downloads/");
+	}
+
+}
